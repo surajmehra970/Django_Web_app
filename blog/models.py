9b8c1459bc6241047e0871b005442cae4cbe9cce
Suraj <Surajmehra2000@gmail.com>
2021-04-08 14:13:00 +0530

    Deleted dummy posts and create sql models

--- blog/models.py
+++ blog/models.py
@@ -9,4 +9,6 @@ class post(models.Model):
 	date_posted = models.DateTimeField(default = timezone.now)
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
 	
+	def __str__(self):
+		return self.title
 
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse #Find what is this
-
+from .models import post
 # Create your views here.
-posts = [
-	{
-		'author':'King',
-		'title': 'Blog post 1',
-		'content': 'Sun of Pride',
-		'date_posted': 'August 27, 2018'
-	},
-	{
-		'author':'Queen',
-		'title': 'Blog post 2',
-		'content': 'Twinkle Queen',
-		'date_posted': 'August 27, 2021'
-	},
-	
-]
 def home(request):
 	context = {
-		'posts':posts
+		'posts':post.objects.all()
 	}
 	return render(request, 'blog/home.html',context)
 
