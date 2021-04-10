237ad9f01621313e89e37827d449d360f5736c40
Suraj <Surajmehra2000@gmail.com>
2021-04-10 14:55:21 +0530

    Created Profile page

--- Web_app_project/settings.py
+++ Web_app_project/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 def register(request):
 	if request.method == 'POST':
@@ -13,3 +14,7 @@ def register(request):
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+	return render(request,'users/profile.html')
