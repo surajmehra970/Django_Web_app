25c97ccfe8921882c70ec052bfa5bf2e16f93808
Suraj <Surajmehra2000@gmail.com>
2021-04-10 13:24:01 +0530

    Created Log In page and Log out page

--- Web_app_project/settings.py
+++ Web_app_project/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'blog-home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
